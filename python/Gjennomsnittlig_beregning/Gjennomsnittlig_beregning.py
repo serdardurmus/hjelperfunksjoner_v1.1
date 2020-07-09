ba4883aa189307b092a6b1b2c24fbdc9f906edb1
serdar <serdar83durmus@gmail.com>
2020-07-09 16:20:13 +0200

    Jeg la til Gjennomsnittlig beregning

--- python/Gjennomsnittlig_beregning/Gjennomsnittlig_beregning.py
+++ python/Gjennomsnittlig_beregning/Gjennomsnittlig_beregning.py
@@ -4,3 +4,9 @@ total = 0
 for a in en_liste:
     total = total + a[1]
 print(total/len(en_liste))
+
+# Under gjennomsnittet
+under_gjennomsnittet = []
+for a,b in en_liste:
+    if (b<(total/len(en_liste))) : under_gjennomsnittet.append([a,b])
+print(under_gjennomsnittet)
