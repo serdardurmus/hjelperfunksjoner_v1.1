6eb1047681be431e75fda7bb86921f418d313ec0
serdar <serdar83durmus@gmail.com>
2020-07-27 21:42:55 +0200

    øvelsegjørmester.py er oppdatert

--- python/øvelsegjørmester.py
+++ python/øvelsegjørmester.py
@@ -1,4 +1,12 @@
-
+# nesned dict
+"""scores= {'Student-1': {'Lesson-1': 57, 'Lesson-2': 46, 'Lesson-3': 58, 'Lesson-4': 81, 'Lesson-5': 85}, 'Student-2': {'Lesson-1': 85, 'Lesson-2': 56, 'Lesson-3': 51, 'Lesson-4': 69, 'Lesson-5': 67}, 'Student-3': {'Lesson-1': 68, 'Lesson-2': 76, 'Lesson-3': 87, 'Lesson-4': 57, 'Lesson-5': 56}, 'Student-4': {'Lesson-1': 78, 'Lesson-2': 93, 'Lesson-3': 88, 'Lesson-4': 38, 'Lesson-5': 54}, 'Student-5': {'Lesson-1': 50, 'Lesson-2': 46, 'Lesson-3': 78, 'Lesson-4': 81, 'Lesson-5': 75}}
+max_avg = 0
+avg = []
+for i in scores:
+    avg.append((i, sum(scores[i].values())/len(scores[i].items())))
+print("Minimum average score and the looser student is ", min(avg, key = lambda x : x[1]))
+print("Maximum average score and the vinner student is ", max(avg, key = lambda x : x[1]))
+"""
 
 # liste1 er inne i liste2 eller ikke
 """liste1 = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[10, 11, 12],[13, 14, 15],[16, 17, 18],
