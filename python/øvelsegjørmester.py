fd01a2a62af5dff29bd1cc1a33f8dfda5ead8920
serdar <serdar83durmus@gmail.com>
2020-07-28 14:30:41 +0200

    Noen filer er oppdatert

--- python/øvelsegjørmester.py
+++ python/øvelsegjørmester.py
@@ -1,4 +1,15 @@
-
+# avarage, dict
+"""n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+for i in student_marks:
+    if i == query_name :
+        a = sum(student_marks[i])/len(student_marks[i])
+        print("%.2f" %a)"""
 
 
 # nested list
