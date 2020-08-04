895a832c5f77856d4e97000cd0f24df3e0d8b007
serdar <serdar83durmus@gmail.com>
2020-08-04 20:03:45 +0200

    Noen filer er oppdatert

--- python/øvelsegjørmester.py
+++ python/øvelsegjørmester.py
@@ -1,3 +1,46 @@
+class PlayStation(object):
+    #model = "PS4"
+    #price = 99
+    def __init__(self, model, price):
+        "initialize properties"
+        #pass
+        self.model = model
+        self.__price = price
+    def set_price(self, price):
+        self.__price = price
+    def get_price(self):
+        return self.__price
+    def save_more(self):
+            if self.amount >= self.__price:
+                self.buying_message = "Yippee! You can buy your " + self.model
+            elif self.amount > self.__price/2:
+                self.buying_message = "You saved more than half, keep saving!"
+            elif self.amount <= self.__price/2:
+                self.buying_message = "You must save more, keep saving!"
+model = "PS4"
+price = 99
+playstation1 = PlayStation(model, price)
+playstation1.set_price(price)
+#playstation1.amount=25
+#playstation1.amount=50
+playstation1.amount=100
+playstation1.save_more()
+print(playstation1.buying_message)
+
+
+
+
+# textwrap, def
+"""import textwrap
+
+def wrap(string, max_width):
+    a = textwrap.fill(string, max_width)
+    return a
+if __name__ == '__main__':
+    string, max_width = "ABCDEFGHIJKLIMNOQRSTUVWXYZ", 4
+    result = wrap(string, max_width)
+    print(result)"""
+
 # You are given a string S.
 # Your task is to find out if the string S contains: alphanumeric characters, 
 # alphabetical characters, digits, lowercase and uppercase characters.
