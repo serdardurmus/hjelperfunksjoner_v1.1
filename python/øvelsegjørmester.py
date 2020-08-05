23881e61045fbfe2d12f00926b0d9a987181ba61
serdar <serdar83durmus@gmail.com>
2020-08-05 21:24:05 +0200

    øvelsegjørmester er oppdatert

--- python/øvelsegjørmester.py
+++ python/øvelsegjørmester.py
@@ -1,3 +1,25 @@
+# OOP Tutorial_2
+"""class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+print(emp_1)
+print(emp_2)
+
+
+# print(emp_1.mail)
+print(emp_2.fullname())
+print(Employee.fullname(emp_1))"""
 
 # OOP Tutorial_1
 """class PlayStation(object):
