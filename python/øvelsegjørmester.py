f3809631066017ccba851650806f8c36db9298a1
serdar <serdar83durmus@gmail.com>
2020-08-05 21:45:23 +0200

    øvelsegjørmester er oppdatert

--- python/øvelsegjørmester.py
+++ python/øvelsegjørmester.py
@@ -1,3 +1,43 @@
+# OOP Tutorial_4
+
+
+
+# OOP Tutorial_3
+"""class Employee:
+
+    num_of_emps = 0
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+print(Employee.num_of_emps)
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+print(Employee.num_of_emps)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# print(emp_1.__dict__)
+# print(Employee.__dict__)
+
+Employee.raise_amount = 1.05
+print(Employee.raise_amount)"""
+
+
 # OOP Tutorial_2
 """class Employee:
 
