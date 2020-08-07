21c98ce85deae7b95988425a4b13aea96e40cd9c
serdar <serdar83durmus@gmail.com>
2020-08-07 22:21:05 +0200

    en file er oppdatert

--- python/øvelsegjørmester_2.py
+++ python/øvelsegjørmester_2.py
@@ -4,13 +4,16 @@
 class Square(object):
     edge = 5 # meter
     area = 0
-    def area(self):
+    def area1(self):
         self.area = self.edge*self.edge
         print(self.area)
 s1 = Square()
 print(s1)
 print(s1.edge)
-print(s1.area())
+print(s1.area1())
+
+s1.edge = 7
+s1.area1()
 
 
 # %% 
