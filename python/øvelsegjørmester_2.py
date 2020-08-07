62bdacca782f167322ee3b1846c74e59f1f8d94c
serdar <serdar83durmus@gmail.com>
2020-08-07 17:57:32 +0200

    Jupyter uploaded

---
+++ python/øvelsegjørmester_2.py
@@ -0,0 +1,32 @@
+
+# %%
+# OOP Tutorial_1
+
+first_name = "serdar"
+last_name = "durmus"
+mail = "serdardurmus@gmail.com"
+
+class Employee(object):
+    def __init__(self, first_name, last_name, mail):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.mail = mail
+
+emp1 = Employee("Serdar","Durmus", "serdardurmus@gmail.com")
+
+# %%
+# OOP Tutorial_1
+
+first_name = "serdar"
+last_name = "durmus"
+mail = "serdardurmus@gmail.com"
+
+class Employee(object):
+    def __init__(self, first_name, last_name, mail):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.mail = mail
+
+emp1 = Employee("Serdar","Durmus", "serdardurmus@gmail.com")
+
+# %%
