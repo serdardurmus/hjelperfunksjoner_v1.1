14f37c9b139843acecfc31e4374371dd2981578e
serdar <serdar83durmus@gmail.com>
2020-08-08 22:36:58 +0200

    OOP eksample er inne

--- python/øvelsegjørmester_2_OOP.py
+++ python/øvelsegjørmester_2_OOP.py
@@ -1,4 +1,60 @@
 # %% 
+# Finaly Project
+
+from abc import ABC, abstractmethod
+# inheritance
+class Shape(ABC):
+    "Shape = super class / abstraction class"
+    # abstraction method
+    @abstractmethod
+    def area(self): pass
+    @abstractmethod
+    def perimeter(self): pass
+    # overriding and polymorphism
+    def toString(self): pass
+
+# child
+class Square(Shape):
+    "sub class"
+    def __init__(self, edge):
+        self.__edge = edge  # encapsulation private attribute
+    def area(self):
+        result = self.__edge ** 2
+        print("Square area: ", result)
+    def perimeter(self):
+        result = self.__edge *4
+        print("Square perimeter: ", result)
+    # overriding and polymoprfhism
+    def toString(self):
+        print("Square edge:", self.__edge)
+
+class Circle(Shape):
+    "Circle class"
+    PI = 3.14 # constant variable
+    def __init__(self, radius):
+        self.__radius = radius
+    def area(self):
+        result = self.PI * self.__radius ** 2 # pi*r**2
+        print("Circle area: ", result)
+    def perimeter(self):
+        result = 2 * self.PI * self.__radius
+        print("Circle perimeter: ", result)
+    # overriding and polymoprfhism
+    def toString(self):
+        print("Circle edge:", self.__radius)
+
+c = Circle(5)
+c.area()
+c.perimeter()
+c.toString()
+
+s = Square(5)
+s.area()
+s.perimeter()
+s.toString()
+
+
+# %% 
 # Polymorphism
 
 class Employee:
