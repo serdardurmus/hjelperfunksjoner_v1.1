2645015a12a927ba0d2b4ca7e445259623f14502
serdar <serdar83durmus@gmail.com>
2020-08-10 20:29:57 +0200

    Det har begynnes vehicle.py

---
+++ python/OOP/vehicle_eksemple_1/vehicle.py
@@ -0,0 +1,18 @@
+
+# parent class
+class VehicleRent:
+
+    def __init__(self, stock):
+        pass
+    def displayStock(self):
+        "display Stock"
+        pass
+    def rentHourly(self, n):
+        "rent Hourly"
+        pass
+    def rentDaily(self, n):
+        "rent rentDaily"
+        pass
+    def returnVehicle(self, request, brand):
+        "return a bill"
+        pass
