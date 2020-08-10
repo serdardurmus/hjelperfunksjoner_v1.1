9ff24085de96dd4aefe56161c4a50fa3c1b91643
serdar <serdar83durmus@gmail.com>
2020-08-10 20:45:58 +0200

    vehicle.py er oppdatert

--- python/OOP/vehicle_eksemple_1/vehicle.py
+++ python/OOP/vehicle_eksemple_1/vehicle.py
@@ -1,21 +1,34 @@
+import datetime
 
 # parent class
 class VehicleRent:
 
     def __init__(self, stock):
-        pass
+        self.stock = stock
+        self.now = 0
+
     def displayStock(self):
         "display Stock"
-        pass
+        print("{} vehicle available to rent".format(self.stock))
+        return self.stock
     def rentHourly(self, n):
         "rent Hourly"
-        pass
+        if n <=0: 
+            print("Number should be positive")
+            return None
+        elif n > self.stock:
+            print("Sorry {} vehicle available to rent".format(self.stock))
+            return None
+        else:
+            self.now = datetime.datetime.now()
+
     def rentDaily(self, n):
         "rent rentDaily"
         pass
     def returnVehicle(self, request, brand):
         "return a bill"
         pass
+        
 
 # child class 1
 class CarRent(VehicleRent):
@@ -23,6 +36,7 @@ class CarRent(VehicleRent):
     def __init__():
         pass
     def discount():
+        "discount "
         pass
 
 # child class 2
@@ -36,6 +50,8 @@ class Customer:
     def __init__():
         pass
     def requestVehicle():
+        "request vehicle"
         pass
     def returnVehicle():
+        "return vehicle"
         pass
