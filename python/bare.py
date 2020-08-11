4ec1dedc04f11a1b322502b52525b66dc9d80f5e
serdar <serdar83durmus@gmail.com>
2020-08-11 19:34:51 +0200

    øvelsegjørmester_2_OOP er oppdatert

--- python/bare.py
+++ python/bare.py
@@ -1,20 +1,20 @@
-N = int(input())
-listem = []
-for i in range(N):
-    kode = (input("Tast inn din kode: ")).split()
-    if kode[0] == "insert": 
-        listem.insert(int(kode[1]), int(kode[2]))
-    elif kode[0] == "print": 
-        print(listem)
-    elif kode[0] == "remove":
-        listem.remove(int(kode[1]))
-    elif kode[0] == "append":
-        listem.append(int(kode[1]))
-    elif kode[0] == "sort":
-        listem.sort()
-    elif kode[0] == "pop":
-        listem.pop()
-    elif kode[0] == "reverse":
-        listem.reverse()
+class Employee:
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
 
-print(listem)
+class Developer(Employee):
+    raise_amt = 1.10
+    def __init__(self, first, last, pay, prog_language):
+        super().__init__(first, last, pay)
+        self.prog_language = prog_language
+
+dev_1 = Developer("Martin", "Lane", 5000, "Python")
+# dev_2 = Developer("Martin2", "Lane2", 4000)
+
+print(dev_1.first)
--- python/øvelsegjørmester_2_OOP.py
+++ python/øvelsegjørmester_2_OOP.py
@@ -1,3 +1,25 @@
+# %%
+class Employee:
+    raise_amt = 1.04
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+class Developer(Employee):
+    raise_amt = 1.10
+    def __init__(self, first, last, pay, prog_language):
+        super().__init__(first, last, pay)
+        self.prog_language = prog_language
+
+dev_1 = Developer("Martin", "Lane", 5000, "Python")
+# dev_2 = Developer("Martin2", "Lane2", 4000)
+
+print(dev_1.first)
+
 # %% 
 # Finaly Project
 
