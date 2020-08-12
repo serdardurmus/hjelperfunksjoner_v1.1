b1ecc7e7d483b4a3d0992b8af2f445d4d7028dcc
serdar <serdar83durmus@gmail.com>
2020-08-12 17:59:01 +0200

    alphabet_rangoli.py er inne

--- python/bare.py
+++ python/bare.py
@@ -1,6 +1,21 @@
-def print_rangoli(size):
-    # your code goes here
+import math
+import os
+import random
+import re
+import sys
 
+# Complete the solve function below.
+def solve(s):
+    for x in s[:].split():
+        s = s.replace(x, x.capitalize())
+    return s
 if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
