fea4535321ba28e8eca7d707b9d8f36aab01d072
serdar <serdar83durmus@gmail.com>
2020-08-12 11:25:49 +0200

    tallsystemer.py er inne

--- python/øvelse/øvelsegjørmester2.py
+++ python/øvelse/øvelsegjørmester2.py
@@ -1,3 +1,37 @@
+# String Formatting
+# Decimal
+# Octal
+# Hexadecimal (capitalized)
+# Binary
+"""def print_formatted(number):
+    w=len(bin(number)[2:])
+    for i in range(1, number+1):
+        print(str(i).rjust(w) , oct(i)[2:].center(w), hex(i)[2:].center(w), bin(i)[2:].center(w))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
+# løsning 2
+def print_formatted(number):
+    results = []
+
+    for i in range(1, n+1):
+        decimal = str(i)
+        octal = str(oct(i)[2:])
+        hex_ = str(hex(i)[2:]).upper()
+        binary = str(bin(i)[2:])
+
+        results.append([decimal, octal, hex_, binary])
+
+    width = len(results[-1][3])  # largest binary number
+
+    for i in results:
+        print(*(rep.rjust(width) for rep in i))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)"""
+
 
 # Text Alignment
 """thickness = int(input()) #This must be an odd number
