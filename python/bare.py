ac26779df3e4652a770d79163606c193b48471b6
serdar <serdar83durmus@gmail.com>
2020-08-22 15:24:29 +0200

    Filen "html-recap-session" er opprettet

--- python/bare.py
+++ python/bare.py
@@ -1,3 +1,12 @@
 def sweetest_icecream(lst):
     pass
 
+class IceCream():
+    def __init__(self, name, number):
+        self.
+
+ice1 = IceCream("Chocolate", 13)         # value of 23
+ice2 = IceCream("Vanilla", 0)            # value of 5
+ice3 = IceCream("Strawberry", 7)         # value of 17
+ice4 = IceCream("Plain", 18)             # value of 18
+ice5 = IceCream("ChocolateChip", 3)      # value of 8
