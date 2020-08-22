4743b27c59701a576f2ab97f430cb780f51f99a3
serdar <serdar83durmus@gmail.com>
2020-08-23 00:49:33 +0200

    en fil er oppdatert

--- python/bare.py
+++ python/bare.py
@@ -9,4 +9,4 @@ ice1 = IceCream("Chocolate", 13)         # value of 23
 ice2 = IceCream("Vanilla", 0)            # value of 5
 ice3 = IceCream("Strawberry", 7)         # value of 17
 ice4 = IceCream("Plain", 18)             # value of 18
-ice5 = IceCream("ChocolateChip", 3)      # value of 8
+ice5 = IceCream("ChocolateChip", 3)      # value of 8.
