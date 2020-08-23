8dc798203a504feee867685c3f82d0eab82e6ace
serdar <serdar83durmus@gmail.com>
2020-08-23 14:29:02 +0200

    en fil er oppdatert

--- python/øvelse/øvelsegjørmester2.py
+++ python/øvelse/øvelsegjørmester2.py
@@ -1,3 +1,5 @@
+# Deneme
+
 # Write a Python code to draw whiteball and Powerball numbers for Powerball 
 # lottery game. 5 Whiteball numbers are choosen from 01 to 69 and 1 Powerball 
 # number is chosen 1 to 26.
