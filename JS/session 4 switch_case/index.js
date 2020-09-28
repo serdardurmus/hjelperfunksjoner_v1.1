62ffa4b2bbaf5a2dfb901226984d300117e234bc
serdar <serdar83durmus@gmail.com>
2020-09-28 16:33:26 +0200

    session 4 switch_case er ferdig

---
+++ JS/session 4 switch_case/index.js
@@ -0,0 +1,13 @@
+var job = "doctor";
+
+switch (job) {
+    case "teacher":
+        console.log("you are a teacher")
+        break;
+    case "doctor":
+        console.log("you are a doctor");
+        break;
+    default:
+        console.log("Who are you?");
+        break;
+}
