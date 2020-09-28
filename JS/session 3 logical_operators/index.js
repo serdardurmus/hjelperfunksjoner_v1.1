18edf9b6f2cb5be84169c78ae7f29dd1757db03e
serdar <serdar83durmus@gmail.com>
2020-09-28 16:17:08 +0200

    session 3 logical_operators er ferdig

---
+++ JS/session 3 logical_operators/index.js
@@ -0,0 +1,20 @@
+var age = 15;
+var job = "doctor";
+
+if (age >= 20 && age <= 50) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (age < 20 || age > 50) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if (age>25 || job == "doctor") {
+    console.log("Kjempebra")
+} else {
+    console.log("bare bra");
+}
