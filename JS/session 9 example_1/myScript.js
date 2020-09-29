7c574b82958acda1b5922e21625943e8305c9c91
serdar <serdar83durmus@gmail.com>
2020-09-29 21:08:18 +0200

    JS session 9 example_1 last commit

--- JS/session 9 example_1/myScript.js
+++ JS/session 9 example_1/myScript.js
@@ -1,20 +1,77 @@
+// Change The Colors!
 
-const colors = ["red", "blue", "gray", "green", "purple"]
-
-const buttonElement = document.querySelector("#randomColorButton");
-
-buttonElement.addEventListener("click", changeColors);
-// buttonElement.addEventListener("click", () => changeColors());
-// buttonElement.addEventListener("click", function () {
-//     changeColors()
-// });
-
-// Vi velger tilfeldig elementet fra Array.
-function changeColors () {
+const colors = [
+    {
+      name: "red",
+      motto: "Rose Red",
+    },
+    {
+      name: "blue",
+      motto: "Ocean Blue",
+    },
+    {
+      name: "gray",
+      motto: "Smoke Gray",
+    },
+    {
+      name: "green",
+      motto: "Grass Green",
+    },
+    {
+      name: "purple",
+      motto: "Deep Purple",
+    },
+  ];
+  
+  const randomColorButton = document.querySelector("#randomColorButton");
+  const addColorButton = document.querySelector("#setColorButton");
+  
+  addColorButton.addEventListener("click", addColor);
+  randomColorButton.addEventListener("click", changeColors);
+  // randomColorButton.addEventListener("click", () => changeColors());
+  // randomColorButton.addEventListener("click", function () {
+  //   changeColors();
+  // });
+  
+  // 1) Arrayden rastgele element seçmek
+  function changeColors() {
     const randomIndex = Math.floor(Math.random() * colors.length);
-    document.querySelector("body").style.backgroundColor = colors[randomIndex];
+  
+    document.querySelector("#colorName").textContent = colors[randomIndex].name;
+    document.querySelector("#colorDesc").textContent = colors[randomIndex].motto;
+  
+    document.querySelector("body").style.backgroundColor =
+      colors[randomIndex].name;
+  }
+  
+  // 2) Kullanıcının girdiği string'i renk listesine eklemek ve arkaplanı o renk ile güncellemek
+  function addColor() {
+    const userInput = document.querySelector(".colorInput");
+  
+    const colorData = userInput.value.split(":");
+  
+    const colorObject = {
+      name: colorData[0],
+      motto: colorData[1],
+    };
+  
+    // indexOf SADECE flat array (["red", "blue"] / [1, 2, 3]) geçerlidir.
+    // findIndex object arraylerde geçerlidir.
+  
+    if (colors.findIndex((color) => color.name === colorObject.name) === -1) {
+      document.querySelector("body").style.backgroundColor = colorObject.name;
+      document.querySelector("#colorName").textContent = colorObject.name;
+      document.querySelector("#colorDesc").textContent = colorObject.motto;
+  
+      colors.push(colorObject);
+      userInput.value = "";
+      userInput.focus();
+    } else {
+      alert("Renk zaten mevcut!");
+    }
+  }
 
-    document.querySelector("#colorName").innerHTML = colors[randomIndex];
+// Kullanıcı boş bir değer girerse?
+// Kullanıcı geçersiz bir değer girerse?
 
-    console.log(randomIndex);
-}
+// Bir input daha ekleyin: kullanıcıdan hex'a değer #eceff1?
